d77f05551df7a688eca1616497c8650571a391d8
rockyengineer7 <rocky.engineer7@gmail.com>
2023-10-19 22:13:52 +0900

    レビュー指摘事項修正

--- ruby/random.rb
+++ ruby/random.rb
@@ -12,37 +12,22 @@
 
 # また表示結果はアルファベット順にしてください。
 
+
 # 6人を配列で定義する
-members = %w[A B C D E F]
+members = ["A", "B", "C", "D", "E", "F"]
 
-# membersを3-3or2-4でグループ化した後にランダムで出力をする
-def group_members(members)
-  # シャッフル後3-3にグループ分けをしてアルファベット順に並び替えて配列に追加
-  def process_3_3(shuffled_members)
-    shuffled_members.each_slice(3).sort_by(&:first).to_a
-  end
+# membersをシャッフルする
+shuffled_members = members.shuffle
 
-  # シャッフル後2-4にグループ分けをしてアルファベット順に並び替えて配列に追加
-  def process_2_4(shuffled_members)
-    [shuffled_members[0..1].sort, shuffled_members[2..5].sort]
-  end
+# randメソッドを使用し、0か1をランダムに決定する
+choice = rand(2)
 
-  shuffled_members = members.shuffle
-
-  # randメソッドで実行する処理をランダムに決める
-  choice = rand(2)
-
-  result = if choice.zero?
-             process_3_3(shuffled_members)
-           else
-             process_2_4(shuffled_members)
-           end
-
-  # リザルトの配列を出力
-  result.each do |group|
-    p group
-  end
+# 0なら3-3,1なら2-4でグループ分けをする
+if choice == 0
+  result = [shuffled_members[0..2].sort, shuffled_members[3..5].sort].to_a
+else
+  result = [shuffled_members[0..1].sort, shuffled_members[2..5].sort].to_a
 end
 
-# メソッドを実行する
-group_members(members)
+# 結果を出力
+result.each {|group| p group}
