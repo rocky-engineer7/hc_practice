b06ec0bdb75dd65dc3e9b09f85d7471e7b7e0e14
rockyengineer7 <rocky.engineer7@gmail.com>
2023-11-09 06:06:39 +0900

    ステップ１ Suica main.rbの出力内容追加

--- ruby/vending_machine/main.rb
+++ ruby/vending_machine/main.rb
@@ -1,11 +1,19 @@
+# frozen_string_literal: true
+
 # main.rb
-require_relative 'vending_machine'
+require_relative "vending_machine"
 
 # Suicaインスタンスを生成する
 suica_card = Suica.new
+
+# 初期残高を取得
+puts suica_card.balance
 
 # チャージする
 suica_card.charge(1000)
 
 # チャージ残高を取得する
 puts suica_card.balance
+
+# 100円未満のチャージをして、例外を発生させる
+suica_card.charge(99)
