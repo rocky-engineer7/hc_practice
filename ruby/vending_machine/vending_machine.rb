8b3da5d094a0b43359aa7c0ac28c8682b54fb5a1
rockyengineer7 <rocky.engineer7@gmail.com>
2023-11-12 15:22:36 +0900

    ステップ２の実装

--- ruby/vending_machine/vending_machine.rb
+++ ruby/vending_machine/vending_machine.rb
@@ -5,6 +5,9 @@ class Suica
   # 100円未満のチャージを禁止する例外
   class DepositException < StandardError; end
 
+  # @balanceの読み取り専用アクセスを提供する
+  attr_reader :balance
+
   # 預かり金500円を引数で受け取り、＠balanceに代入する
   def initialize(balance = 500)
     @balance = balance
@@ -18,9 +21,39 @@ class Suica
     # 現在の残高とamountを加算してから、@balanceに代入する
     @balance += amount
   end
+end
 
-  # チャージ残高を取得する
-  def balance
-    puts @balance
+# ジュースの種類
+
+# 初期状態でペプシ５本格納
+# 在庫数を取得
+
+# Juiceクラス
+class Juice
+  # name,priceの読み取り専用アクセスを提供する
+  attr_reader :name, :price
+
+  # ジュースの情報
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+
+# VendingMachineクラスの定義
+class VendingMachine
+  # 在庫情報への読み取り専用アクセスを提供する
+  attr_reader :stock
+
+  # 自動販売機の初期状態を設定
+  def initialize
+    pepsi = Juice.new("ペプシ", 150)
+    @stock = { pepsi => 5 }
+  end
+
+  # 在庫情報を出力するメソッド
+  def stock_info
+    # 在庫ハッシュの各要素に対して繰り返し処理を行う
+    @stock.map { |juice, count| "#{juice.name} - 価格: #{juice.price}円, 在庫: #{count}本" }
   end
 end
